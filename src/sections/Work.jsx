d79d9ed4b661cec7d873525a65d2b64e24f07d8d
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-06-10 11:29:53 -0500

    Updated card grid title styles and link placement

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -25,29 +25,23 @@ const Work = () => {
       <div className={styles.contentCotainer}>
         {data?.map((value) => {
           return (
-            <div className={styles.card} key={value.id}>
-              <div className={styles.imgContainer}>
-                <img src={value.image} alt="" className={styles.img} />
-              </div>
+            <a
+              href={value.demoLink}
+              target="_blank"
+              rel="noreferrer"
+              key={value.id}
+            >
+              <div className={styles.card}>
+                <div className={styles.imgContainer}>
+                  <img src={value.image} alt="" className={styles.img} />
+                </div>
 
-              <div className={styles.titleContainer}>
-                <p className={styles.title}>{value.name}</p>
-                <a href={value.demoLink} target="_blank" rel="noreferrer">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7Z"
-                    />
-                  </svg>
-                </a>
+                <div className={styles.titleContainer}>
+                  <p className={styles.title}>{value.name}</p>
+                </div>
+                <p className={styles.description}>{value.description}</p>
               </div>
-              <p className={styles.description}>{value.description}</p>
-            </div>
+            </a>
           );
         })}
       </div>
